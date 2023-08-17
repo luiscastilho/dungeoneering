b3eb842c5bdc0564fb537f872412267e36ea7454
Luis Castilho <luiscastilho@gmail.com>
2023-08-17 12:34:24 -0300

    Update create release script

--- docs/releases/create_release.sh
+++ docs/releases/create_release.sh
@@ -2,6 +2,23 @@
 #
 # Generate a release from an exported Processing application.
 #
+# By default, dungeoneering is exported in standalone mode. The main folder
+# (dungeoneering) and code file (dungeoneering.pde) have to be renamed to be
+# exported in other modes - dungeoneeringDm/dungeoneeringDm.pde for DM mode and
+# dungeoneeringPlayers/dungeoneeringPlayers.pde for Players' mode. The main
+# code file has to be edited, commenting or uncommenting these lines
+# accordingly:
+#    appMode = AppMode.standalone;
+#    // appMode = AppMode.dm;
+#    // appMode = AppMode.players;
+#
+# When exporting in Players' mode, also swap these lines, so the app will run
+# on the second monitor:
+#    fullScreen(P2D, 1);
+#    // fullScreen(P2D, 2);
+#
+# Also, remember to sync info between index.md and TXT files on docs/releases.
+#
 # Requirements: bash, coreutils, zip
 #
 
@@ -16,7 +33,7 @@ working_dir=""
 working_subdir=""
 
 usage() {
-    echo "usage: $(basename $0) version platform [app_mode]"
+    echo "usage: $(basename "$0") version platform [app_mode]"
     echo "Creates a dungeoneering release"
     echo "version should be vX.Y.Z"
     echo "platform should be windows, linux, or macos"
@@ -26,17 +43,18 @@ usage() {
 
 # Check if we have everything we need
 
-if [ -z "${version}" -o -z "${platform}" ]; then
+if [ -z "${version}" ] || [ -z "${platform}" ]; then
     usage
 fi
-if [ "${platform}" != "windows" -a "${platform}" != "linux" -a "${platform}" != "macos" ]; then
+if [ "${platform}" != "windows" ] && [ "${platform}" != "linux" ] && [ "${platform}" != "macos" ]; then
     usage
 fi
-if [ -n "${app_mode}" -a "${app_mode}" != "standalone" -a "${app_mode}" != "dm" -a "${app_mode}" != "players" ]; then
+if [ -n "${app_mode}" ] && [ "${app_mode}" != "standalone" ] && [ "${app_mode}" != "dm" ] && [ "${app_mode}" != "players" ]; then
     usage
 fi
 
 spaces_regex="[[:space:]]+"
+version_regex="v[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+"
 if [[ "${version}" =~ $spaces_regex ]]; then
     echo "ERROR: version shouldn't contain spaces"
     exit 1
@@ -49,62 +67,63 @@ if [[ "${app_mode}" =~ $spaces_regex ]]; then
     echo "ERROR: app_mode shouldn't contain spaces"
     exit 1
 fi
-
-if [ "${platform}" == "windows" -a ! -d "${root_dir}/dungeoneering/application.windows64/" ]; then
-    echo "ERROR: dir application.windows64 not found"
-    exit 1
-fi
-if [ "${platform}" == "linux" -a ! -d "${root_dir}/dungeoneering/application.linux64/" ]; then
-    echo "ERROR: dir application.linux64 not found"
-    exit 1
-fi
-if [ "${platform}" == "macos" -a ! -d "${root_dir}/dungeoneering/application.macosx/" ]; then
-    echo "ERROR: dir application.macosx not found"
+if [[ ! "${version}" =~ $version_regex ]]; then
+    echo "ERROR: version should be in format vX.Y.Z"
     exit 1
 fi
 
-if [ "${platform}" == "windows" -a -d "${root_dir}/releases/${version}/dungeoneering-windows64/" ]; then
-    echo "ERROR: dir dungeoneering-windows64 already exists"
+if [ "${platform}" == "windows" ] && [ ! -d "${root_dir}/dungeoneering/windows-amd64/" ]; then
+    echo "ERROR: dir windows-amd64 not found"
     exit 1
-fi
-if [ "${platform}" == "linux" -a -d "${root_dir}/releases/${version}/dungeoneering-linux64/" ]; then
-    echo "ERROR: dir dungeoneering-linux64 already exists"
+elif [ "${platform}" == "linux" ] && [ ! -d "${root_dir}/dungeoneering/linux-amd64/" ]; then
+    echo "ERROR: dir linux-amd64 not found"
     exit 1
-fi
-if [ "${platform}" == "macos" -a "${app_mode}" == "standalone" -a -d "${root_dir}/releases/${version}/dungeoneering-macos/" ]; then
-    echo "ERROR: dir dungeoneering-macos already exists"
+elif [ "${platform}" == "macos" ] && [ ! -d "${root_dir}/dungeoneering/macos-x86_64/" ]; then
+    echo "ERROR: dir macos-x86_64 not found"
     exit 1
 fi
 
-echo "Creating release dungeoneering-${platform}64.zip..."
+if [ "${platform}" == "windows" ] && [ -d "${root_dir}/releases/${version}/dungeoneering-windows-amd64/" ]; then
+    echo "ERROR: dir dungeoneering-windows-amd64 already exists"
+    exit 1
+elif [ "${platform}" == "linux" ] && [ -d "${root_dir}/releases/${version}/dungeoneering-linux-amd64/" ]; then
+    echo "ERROR: dir dungeoneering-linux-amd64 already exists"
+    exit 1
+elif [ "${platform}" == "macos" ] && [ "${app_mode}" == "standalone" ] && [ -d "${root_dir}/releases/${version}/dungeoneering-macos-amd64/" ]; then
+    echo "ERROR: dir dungeoneering-macos-amd64 already exists"
+    exit 1
+fi
+
+echo "Creating release dungeoneering-${platform}-amd64.zip..."
 
 # Go to application root dir
 
 cd ${root_dir}
 
-# Exclude uneeded exported applications
+# Exclude unnecessary exported applications
 
-if [ "${platform}" == "windows" ]; then
-    echo -n "  Exclude application.windows32 directory..."
-    if [ -d dungeoneering/application.windows32/ ]; then
-        rm -r dungeoneering/application.windows32/
+if [ "${platform}" == "macos" ]; then
+    if [ -d dungeoneering/macos-aarch64/ ]; then
+        echo -n "  Exclude macos-aarch64 directory..."
+        rm -r dungeoneering/macos-aarch64/
+        echo " done"
     fi
-    echo " done"
 fi
-if [ "${platform}" == "linux" ]; then
-    echo -n "  Exclude application.linux32 directory..."
-    if [ -d dungeoneering/application.linux32/ ]; then
-        rm -r dungeoneering/application.linux32/
-    fi
-    echo " done"
-    echo -n "  Exclude application.linux-arm64 directory..."
-    if [ -d dungeoneering/application.linux-arm64/ ]; then
-        rm -r dungeoneering/application.linux-arm64/
-    fi
-    echo " done"
-    echo -n "  Exclude application.linux-armv6hf directory..."
-    if [ -d dungeoneering/application.linux-armv6hf/ ]; then
-        rm -r dungeoneering/application.linux-armv6hf/
+
+# Fix file permissions
+
+if [ "${platform}" == "linux" ] || [ "${platform}" == "macos" ]; then
+    echo -n "  Fix file permissions..."
+    chmod 644 docs/releases/*.txt
+    chmod 644 docs/*.md
+    if [ "${platform}" == "linux" ]; then
+        find dungeoneering/linux-amd64/data/ -type f -exec chmod 644 {} \;
+        find dungeoneering/linux-amd64/lib/ -type f -exec chmod 644 {} \;
+        find dungeoneering/linux-amd64/source/ -type f -exec chmod 644 {} \;
+    elif [ "${platform}" == "macos" ]; then
+        find dungeoneering/macos-x86_64/data/ -type f -exec chmod 644 {} \;
+        find dungeoneering/macos-x86_64/lib/ -type f -exec chmod 644 {} \;
+        find dungeoneering/macos-x86_64/source/ -type f -exec chmod 644 {} \;
     fi
     echo " done"
 fi
@@ -112,25 +131,23 @@ fi
 # Create version root dir
 
 echo -n "  Create ${version} directory..."
-mkdir -p releases/${version}
+mkdir -p "releases/${version}"
 echo " done"
 
 # Move application dir to version dir
 
 if [ "${platform}" == "windows" ]; then
-    working_dir=dungeoneering-windows64
-    echo -n "  Move application.windows64 to ${working_dir}..."
-    mv dungeoneering/application.windows64 releases/${version}/${working_dir}
+    working_dir=dungeoneering-windows-amd64
+    echo -n "  Move windows-amd64 to ${working_dir}..."
+    mv dungeoneering/windows-amd64 "releases/${version}/${working_dir}"
     echo " done"
-fi
-if [ "${platform}" == "linux" ]; then
-    working_dir=dungeoneering-linux64
-    echo -n "  Move application.linux64 to ${working_dir}..."
-    mv dungeoneering/application.linux64 releases/${version}/${working_dir}
+elif [ "${platform}" == "linux" ]; then
+    working_dir=dungeoneering-linux-amd64
+    echo -n "  Move linux-amd64 to ${working_dir}..."
+    mv dungeoneering/linux-amd64 "releases/${version}/${working_dir}"
     echo " done"
-fi
-if [ "${platform}" == "macos" ]; then
-    working_dir=dungeoneering-macos
+elif [ "${platform}" == "macos" ]; then
+    working_dir=dungeoneering-macos-amd64
     if [ "${app_mode}" == "standalone" ]; then
         working_subdir=dungeoneering.app
     elif [ "${app_mode}" == "dm" ]; then
@@ -138,75 +155,72 @@ if [ "${platform}" == "macos" ]; then
     elif [ "${app_mode}" == "players" ]; then
         working_subdir=dungeoneeringPlayers.app
     fi
-    echo -n "  Move application.macosx to ${working_dir}..."
-    mkdir -p releases/${version}/${working_dir}
-    mv dungeoneering/application.macosx/dungeoneering* releases/${version}/${working_dir}/
-    if [ ! -d releases/${version}/${working_dir}/source ]; then
-        mv dungeoneering/application.macosx/source releases/${version}/${working_dir}/
+    echo -n "  Move macos-x86_64 to ${working_dir}..."
+    mkdir -p "releases/${version}/${working_dir}"
+    mv dungeoneering/macos-x86_64/dungeoneering* "releases/${version}/${working_dir}/"
+    if [ ! -d "releases/${version}/${working_dir}/source" ]; then
+        mv dungeoneering/macos-x86_64/source "releases/${version}/${working_dir}/"
     fi
-    rm -r dungeoneering/application.macosx
+    rm -r dungeoneering/macos-x86_64
     echo " done"
 fi
 
 # Copy release files to version dir
 
 echo -n "  Copy release files to ${working_dir}..."
-cp docs/releases/*.txt releases/${version}/${working_dir}/
-cp docs/CHANGELOG.md releases/${version}/${working_dir}/CHANGELOG.txt
+cp docs/releases/*.txt "releases/${version}/${working_dir}/"
+cp docs/CHANGELOG.md "releases/${version}/${working_dir}/CHANGELOG.txt"
 echo " done"
 
-# Copy libraries to lib dir and then remove platform specific files
+# Copy non-exported libraries to lib dir
 
-echo -n "  Copy libraries to ${working_dir} lib or Java dir..."
-if [ "${platform}" == "windows" -o "${platform}" == "linux" ]; then
-    cp -r dungeoneering/code/* releases/${version}/${working_dir}/lib
-    rm -r releases/${version}/${working_dir}/lib/macosx64/
-    rm -r releases/${version}/${working_dir}/lib/windows32/
-    if [ "${platform}" == "linux" ]; then
-        rm -r releases/${version}/${working_dir}/lib/windows64/
-    fi
-fi
-if [ "${platform}" == "macos" ]; then
-    gcp -r dungeoneering/code/* releases/${version}/${working_dir}/${working_subdir}/Contents/Java/
-    rm -r releases/${version}/${working_dir}/${working_subdir}/Contents/Java/windows32/
-    rm -r releases/${version}/${working_dir}/${working_subdir}/Contents/Java/windows64/
+echo -n "  Copy libraries to ${working_dir}..."
+if [ "${platform}" == "windows" ]; then
+    cp -r dungeoneering/code/shader "releases/${version}/${working_dir}/lib"
+elif [ "${platform}" == "linux" ]; then
+    cp -r dungeoneering/code/shader "releases/${version}/${working_dir}/lib"
+elif [ "${platform}" == "macos" ]; then
+    cp -r dungeoneering/code/shader "releases/${version}/${working_dir}/${working_subdir}/Contents/Java/"
 fi
 echo " done"
 
-# In macOS only, copy data to working dir
+# In macOS, copy data to working dir
 
 if [ "${platform}" == "macos" ]; then
-    if [ ! -d releases/${version}/${working_dir}/data ]; then
+    if [ ! -d "releases/${version}/${working_dir}/data" ]; then
         echo -n "  Copy data to ${working_dir}..."
-        gcp -r dungeoneering/data releases/${version}/${working_dir}/
-        rm -r releases/${version}/${working_dir}/data/conditions/
-        rm -r releases/${version}/${working_dir}/data/cursors/
-        rm -r releases/${version}/${working_dir}/data/fonts/
-        rm -r releases/${version}/${working_dir}/data/icons/
+        gcp -r dungeoneering/data "releases/${version}/${working_dir}/"
+        rm -r "releases/${version}/${working_dir}/data/conditions/"
+        rm -r "releases/${version}/${working_dir}/data/cursors/"
+        rm -r "releases/${version}/${working_dir}/data/fonts/"
+        rm -r "releases/${version}/${working_dir}/data/icons/"
+        echo " done"
     else
         echo -n "  Skipping copy of data to ${working_dir} - already present..."
     fi
 fi
-echo " done"
 
 # Remove unnecessary files from version dir
 
 echo -n "  Remove unnecessary files from ${working_dir}..."
-if [ -f releases/${version}/${working_dir}/source/dungeoneering.java ]; then
-    rm releases/${version}/${working_dir}/source/dungeoneering.java
+if [ -f "releases/${version}/${working_dir}/source/dungeoneering.java" ]; then
+    rm "releases/${version}/${working_dir}/source/dungeoneering.java"
 fi
-if [ -d releases/${version}/${working_dir}/data/icons/orig/ ]; then
-    rm -r releases/${version}/${working_dir}/data/icons/orig/
+if [ -d "releases/${version}/${working_dir}/data/icons/orig/" ]; then
+    rm -r "releases/${version}/${working_dir}/data/icons/orig/"
 fi
-if [ -d releases/${version}/${working_dir}/data/campaign/ ]; then
-    rm -r releases/${version}/${working_dir}/data/campaign/
+if [ -d "releases/${version}/${working_dir}/data/campaign/" ]; then
+    rm -r "releases/${version}/${working_dir}/data/campaign/"
+fi
+if [ -d "releases/${version}/${working_dir}/log/" ]; then
+    rm -r "releases/${version}/${working_dir}/log/"
 fi
 echo " done"
 
 # Create release zip file
 
 echo -n "  Create a ZIP file with ${working_dir} contents..."
-cd releases/${version}
+cd "releases/${version}"
 zip -qr ${working_dir}.zip ${working_dir}/
 echo " done"
 
